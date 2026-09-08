2e3ae8a251667ea8296cad3f5ffe2c93295607db
Dulanjna Rathnayaka <dulanjanarmd@gmail.com>
2026-09-08 16:27:10 +0530

    feat: add UserController to expose endpoint for retrieving all users

--- src/main/java/com/prismo/backend/controller/UserController.java
+++ src/main/java/com/prismo/backend/controller/UserController.java
@@ -1,24 +1,25 @@
 package com.prismo.backend.controller;
 
 import com.prismo.backend.model.User;
-import com.prismo.backend.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.prismo.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "*", maxAge = 3600)
+@RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    private UserService userService;
+
+    private final UserRepository userRepository;
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
 }
